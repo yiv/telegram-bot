7e5f37d06fa86d7807f59a966afcba8238116bf9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-24 18:47:13 +0000

    Deploy telegram-rs/telegram-bot to github.com/telegram-rs/telegram-bot.git:gh-pages

---
+++ implementors/serde/de/trait.IntoDeserializer.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {};
+implementors["serde"] = [];
+implementors["serde_json"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"serde/de/trait.IntoDeserializer.html\" title=\"trait serde::de::IntoDeserializer\">IntoDeserializer</a>&lt;'de, <a class=\"struct\" href=\"serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"serde_json/value/enum.Value.html\" title=\"enum serde_json::value::Value\">Value</a>","synthetic":false,"types":["serde_json::value::Value"]}];
+implementors["serde_value"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"serde/de/trait.IntoDeserializer.html\" title=\"trait serde::de::IntoDeserializer\">IntoDeserializer</a>&lt;'de, <a class=\"enum\" href=\"serde_value/enum.DeserializerError.html\" title=\"enum serde_value::DeserializerError\">DeserializerError</a>&gt; for <a class=\"enum\" href=\"serde_value/enum.Value.html\" title=\"enum serde_value::Value\">Value</a>","synthetic":false,"types":["serde_value::Value"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
